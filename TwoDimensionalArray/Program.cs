709ce19491fc8837ce62470549b21c2174ad101e
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-17 11:40:49 +0900

    foreachを使ったTwoDimenionalArrayというプログラムを追加

---
+++ TwoDimensionalArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] teams =
+            {
+                new string[] {"player1", "player2", "player3" },
+                new string[] {"player4", "player5" },
+                new string[] {"player6", "player7", "player8"},
+                new string[] {"player9"}
+            };
+
+            foreach (string[] team in teams)
+            {
+                foreach (string playerName in team)
+                {
+                    Console.Write(playerName + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
