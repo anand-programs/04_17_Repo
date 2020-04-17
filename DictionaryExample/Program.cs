9d573e2b8f7d465b4f5c10e2f87314d5b914da30
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-17 17:33:56 +0900

    DictionaryExampleというプログラムを追加

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;                                                               //nameclass for using Dictionary
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animalsDictionary = new Dictionary<string, string>();                           //key & value both string type
+            animalsDictionary.Add("cage", "sparrow");                                           //Add method of dictionary
+            animalsDictionary.Add("den", "lion");
+            animalsDictionary.Add("aquarium", "fish");
+
+            Console.WriteLine("Number of entries in dictionary: " + animalsDictionary.Count);   //Count property of dictionary
+            foreach(var animal in animalsDictionary)                                            //var can be replaced by KeyValuePair<string, string>
+            {
+                Console.WriteLine(animal.Value + " is inside " + animal.Key);
+            }
+        }
+    }
+}
