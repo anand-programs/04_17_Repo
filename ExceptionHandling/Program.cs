6bd59817aa983ef1557d3d46d7be3b2341ee3c8a
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-20 10:17:09 +0900

    ExceptionHandlingというプログラムを追加

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Successfully executed");
+
+            try
+            {
+                int number = 0;
+                int answer = 5 / number;                // Exception, so catch will be executed
+                Console.WriteLine(answer);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);           // Message property of object e
+            }
+
+            Console.WriteLine("Successfully executed");
+        }
+    }
+}
