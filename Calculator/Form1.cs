5e9e8f0c7e4ff8f5e41c7343e043f3626ce854ee
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-21 11:37:12 +0900

    電卓の全てのボタンのメソッドの内容は完成です。"="の内容だけまだです。

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -30,17 +30,31 @@ namespace Calculator
         #region Delete Methods
         private void CEButton_Click(object sender, EventArgs e)
         {
+            //Clears text from user input box
+            this.UserInput.Text = string.Empty;
+            //or just "" is also ok
 
+            //Focus the cursor
+            FocusInputText();
         }
 
+        //CE and C buttons have the same funciton in this calculator
         private void CButton_Click(object sender, EventArgs e)
         {
+            //Clears text from user input box
+            this.UserInput.Text = string.Empty;
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void DeleteButton_Click(object sender, EventArgs e)
         {
+            //Delete the character to the right of  cursor
+            DeleteTextValue();
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         #endregion
@@ -49,93 +63,203 @@ namespace Calculator
 
         private void DivisonButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("/");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void MultiplicationButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("*");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void SubtractionButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("-");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void AdditionButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("+");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void EqualsButton_Click(object sender, EventArgs e)
         {
-
+            CalculateEquation();
         }
 
         private void SignButton_Click(object sender, EventArgs e)
         {
+            //Insert a minus sign if nothing has been inserted
+            if (this.UserInput.Text.Length == 0)
+            {
+                InsertTextValue("-");
+            }
 
+            //Delete the minus sign (make it positive) if a minus has already been inserted
+            else if(this.UserInput.Text == "-")
+            {
+                //Clear the minus sign
+                this.UserInput.Text = string.Empty;
+            }
+
+            //Focus the cursor
+            FocusInputText();
         }
 
         #endregion
 
         #region Numbers Method
 
+        //Inserts the specified number
         private void DecimalButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue(".");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void ZeroButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("0");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void OneButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("1");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void TwoButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("2");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void ThreeButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("3");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void FourButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("4");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void FiveButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("5");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void SixButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("6");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void SevenButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("7");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void EightButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("8");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         private void NineButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("9");
 
+            //Focus the cursor
+            FocusInputText();
         }
 
         #endregion
+
+        #region Private Helping Methods
+
+        private void FocusInputText()
+        {
+            this.UserInput.Focus();
+        }
+
+        //Insert at the cursor
+        private void InsertTextValue(string value)
+        {
+            //Remember selection start
+            var selectionStart = this.UserInput.SelectionStart;
+
+            //Set new text
+            this.UserInput.Text = this.UserInput.Text.Insert(this.UserInput.SelectionStart, value);
+
+            //Restore selection start
+            this.UserInput.SelectionStart = selectionStart + value.Length;
+
+            //Set selection lenght to 0 (selecting a part will focus the cursor to start of selection)
+            this.UserInput.SelectionLength = 0;
+        }
+
+        //Deletes a character to the right of the cursor
+        private void DeleteTextValue()
+        {
+            //If there is nothing to delete
+            if (this.UserInput.Text.Length < this.UserInput.SelectionStart + 1)
+                return;
+
+            //Remember selection start
+            var selectionStart = this.UserInput.SelectionStart;
+
+            //Dlete the character to the right of the cusror
+            this.UserInput.Text = this.UserInput.Text.Remove(this.UserInput.SelectionStart, 1);
+
+            //Restore selection start
+            this.UserInput.SelectionStart = selectionStart;
+
+            //Set selection lenght to 0 (selecting a part will focus the cursor to start of selection)
+            this.UserInput.SelectionLength = 0;
+        }
+
+        #endregion
+
+        private void CalculateEquation()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
